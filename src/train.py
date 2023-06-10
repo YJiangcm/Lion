e46c3ea137106fd0dbb67746da5d291b6a070070
Yuxin Jiang 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2023-06-10 16:13:59 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -51,6 +51,15 @@ class TrainingArguments(transformers.TrainingArguments):
     )
 
 
+def safe_save_model_for_hf_trainer(trainer: transformers.Trainer, output_dir: str):
+    """Collects the state dict and dump to disk."""
+    state_dict = trainer.model.state_dict()
+    if trainer.args.should_save:
+        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
+        del state_dict
+        trainer._save(output_dir, state_dict=cpu_state_dict)  # noqa
+
+
 def smart_tokenizer_and_embedding_resize(
     special_tokens_dict: Dict,
     tokenizer: transformers.PreTrainedTokenizer,
@@ -166,31 +175,6 @@ def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, dat
     train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path)
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
     return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
-
-
-###############################################################################################
-# to reduce the GPU Memory-Usage
-class CustomTrainer(Trainer):
-    def save_model(self, output_dir: Optional[str] = None, _internal_call: bool = False):
-        if self.fsdp is not None:
-            if output_dir is None:
-                output_dir = self.args.output_dir
-            from torch.distributed.fsdp import (
-                FullyShardedDataParallel as FSDP,
-                FullStateDictConfig,
-                StateDictType,
-            )
-            save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
-            with FSDP.state_dict_type(self.model, StateDictType.FULL_STATE_DICT, save_policy):
-                cpu_state_dict = self.model.state_dict()
-            if self.args.should_save:
-                self._save(output_dir, state_dict=cpu_state_dict)  # noqa
-            # Push to the Hub when `save_model` is called by the user.
-            if self.args.push_to_hub and not _internal_call:
-                self.push_to_hub(commit_message="Model save")
-        else:
-            super().save_model(output_dir, _internal_call)
-###############################################################################################
     
 
 def train():
@@ -227,15 +211,16 @@ def train():
 
     data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
     
-    # replace Trainer
-    trainer = CustomTrainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
-    if training_args.resume_from_checkpoint:
-        trainer.train(resume_from_checkpoint=True)
-    else:
-        trainer.train()
+   # Tell Trainer not to attempt DataParallel
+    model.is_parallelizable = True
+    model.model_parallel = True
+
+    trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
+    model.config.use_cache = False
+
+    trainer.train()
     trainer.save_state()
-    print("ready to save model")
-    trainer.save_model(output_dir=training_args.output_dir)
+    safe_save_model_for_hf_trainer(trainer=trainer, output_dir=training_args.output_dir)
 
 
 if __name__ == "__main__":
