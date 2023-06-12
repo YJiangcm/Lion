13aff9d82c6e5ae1517020bede8a588e6ca19500
Yuxin Jiang 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2023-06-12 15:26:11 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -21,12 +21,12 @@ PROMPT_DICT = {
     "prompt_input": (
         "Below is an instruction that describes a task, paired with an input that provides further context. "
         "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
+        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
     ),
     "prompt_no_input": (
         "Below is an instruction that describes a task. "
         "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Response:"
+        "### Instruction:\n{instruction}\n\n### Response:\n"
     ),
 }
 
