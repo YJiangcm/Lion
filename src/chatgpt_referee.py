40655ad26d2cab1ca9082460511842f43e7cfa29
Yuxin Jiang 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2023-10-13 23:14:03 +0800

    Update chatgpt_referee.py

--- src/chatgpt_referee.py
+++ src/chatgpt_referee.py
@@ -43,41 +43,54 @@ def get_eval(user_prompt: str, max_tokens: int, api_key: str):
     logger.error(f'Failed after {MAX_API_RETRY} retries.')
     return 'error'
 
-
+    
 def parse_score(review):
     try:
-        score_pair = review.split('\n')[0]
-        score_pair = score_pair.replace(',', ' ')
-        sp = score_pair.split(' ')
-        if len(sp) == 2:
-            return [float(sp[0]), float(sp[1])]
+        score1 = review.split("\n")[-2]
+        score2 = review.split("\n")[-1]
+
+        if "Assistant 1" in score1.split(":")[0]:
+            score1 = score1.split(":")[-1].strip()
         else:
-            raise Exception('Invalid score pair.')
-    except Exception as e:
-        logger.error(f'{e}\nContent: {review}\n'
-                     'You must manually fix the score pair.')
+            print(f'Failed to parse scores from {review}')
+            return [-1, -1]
+
+        if "Assistant 2" in score2.split(":")[0]:
+            score2 = score2.split(":")[-1].strip()
+        else:
+            print(f'Failed to parse scores from {review}')
+            return [-1, -1]
+        
+        return [float(score1), float(score2)]
+    
+    except:
+        print(f'Failed to parse scores from {review}')
         return [-1, -1]
 
 
-def gen_prompt(ques, input, ans1, ans2):
+def gen_prompt(ques, ans1, ans2):
     
-    prompt_template = "[Question]\n{question}\n\n[Input]\n{input}\n\n[The Start of Assistant 1's Answer]\n{answer_1}\n\n[The End of Assistant 1's Answer]\n\n" \
-        + "[The Start of Assistant 2's Answer]\n{answer_2}\n\n[The End of Assistant 2's Answer]\n\n[System]\n{prompt}\n\n"
+    prompt_template = "[Instruction]\n{instruction}\n\n[The Start of Assistant 1's Answer]\n{answer_1}\n[The End of Assistant 1's Answer]\n\n[The Start of Assistant 2's Answer]\n{answer_2}\n[The End of Assistant 2's Answer]\n\n[System]\n{prompt}\n"
     
-    evaluate_prompt = "We would like to request your feedback on the performance of two AI assistants in response to the user question and input displayed above." \
-        + "\nPlease rate the helpfulness, relevance, accuracy, level of details of their responses. " \
-        + "Each assistant receives an overall score on a scale of 1 to 10, where a higher score indicates better overall performance." \
-        + "\nPlease first output a single line containing only two values indicating the scores for Assistant 1 and 2, respectively. The two scores are separated by a space. " \
-        + "In the subsequent line, please provide a comprehensive explanation of your evaluation, avoiding any potential bias and ensuring that the order in which the responses were presented does not affect your judgment."
+    default_prompt =  """We would like to request your feedback on the performance of two AI assistants in response to the user instruction displayed above.
     
-    prompt = prompt_template.format(question=ques, input=input, answer_1=ans1, answer_2=ans2, prompt=evaluate_prompt)
+    Please rate the helpfulness, relevance, accuracy, level of details of their responses. Each assistant receives an overall score on a scale of 1 to 10, where a higher score indicates better overall performance.
+
+    Please first provide a comprehensive explanation of your evaluation, avoiding any potential bias and ensuring that the order in which the responses were presented does not affect your judgment. Then, output two lines indicating the scores for Assistant 1 and 2, respectively.
+
+    Output with the following format:
+    Evaluation evidence: <your evaluation explanation here>
+    Score of the Assistant 1: <score>
+    Score of the Assistant 2: <score>"""
+    
+    prompt = prompt_template.format(instruction=ques, answer_1=ans1, answer_2=ans2, prompt=default_prompt)
 
     return prompt
 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ChatGPT-referee to compare the quality of two responses.')
+    parser = argparse.ArgumentParser(description='ChatGPT-based QA evaluation.')
     parser.add_argument('-a', '--answer-file-list', nargs='+', default=[])
     parser.add_argument('-o', '--output-review-file')
     parser.add_argument('--api_key', type=str)
@@ -94,16 +107,15 @@ if __name__ == '__main__':
     question_idx_list = list(range(len(question_jsons)))
     
     ques = [i['instruction'] for i in question_jsons]
-    input = [i['input'] for i in question_jsons]
     ans1 = [i['output'] for i in answer1_jsons]
     ans2 = [i['output'] for i in answer2_jsons]
     
     # if not args.recheck:
-        ### first run
+    ### first run
     ray.init()
     handles = []
     for idx in tqdm(question_idx_list):
-        user_prompt = gen_prompt(ques[idx], input[idx], ans1[idx], ans2[idx])
+        user_prompt = gen_prompt(ques[idx], ans1[idx], ans2[idx])
         
         # To avoid the rate limit set by OpenAI
         handles.append(get_eval.remote(user_prompt, args.max_tokens, args.api_key))
@@ -121,20 +133,20 @@ if __name__ == '__main__':
     ray.shutdown()
             
     # else:
-        ### recheck
+    ### recheck
     old_question_idx_list = question_idx_list
     
     reviews = get_json_list2(args.output_review_file)
     new_question_idx_list = []
     for idx in range(len(reviews)):
-        if reviews[idx]['score']==[-1, -1]:
+        if parse_score(reviews[idx]['text']) == [-1, -1]:
             new_question_idx_list.append(idx)
             
     while len(new_question_idx_list) < len(old_question_idx_list):
         ray.init()
         handles = []
         for idx in tqdm(new_question_idx_list):
-            user_prompt = gen_prompt(ques[idx], input[idx], ans1[idx], ans2[idx])
+            user_prompt = gen_prompt(ques[idx], ans1[idx], ans2[idx])
             # To avoid the rate limit set by OpenAI
             handles.append(get_eval.remote(user_prompt, args.max_tokens, args.api_key))
             logger.info(f'Waiting for {REQ_TIME_GAP} seconds before sending the next request.')
@@ -155,8 +167,7 @@ if __name__ == '__main__':
         
         new_question_idx_list = []
         for idx in range(len(reviews)):
-            if reviews[idx]['score']==[-1, -1]:
+            if parse_score(reviews[idx]['text']) == [-1, -1]:
                 new_question_idx_list.append(idx)
 
         ray.shutdown()
-    
