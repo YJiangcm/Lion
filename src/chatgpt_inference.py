fa2a3b1cf590be904ff457ac984bb62bd617f587
Yuxin Jiang 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2023-10-13 16:11:56 +0800

    Update chatgpt_inference.py

--- src/chatgpt_inference.py
+++ src/chatgpt_inference.py
@@ -10,8 +10,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-from utils import get_json_list
-
 MAX_API_RETRY = 5
 REQ_TIME_GAP = 20
 
@@ -43,46 +41,40 @@ def get_eval(user_prompt: str, max_tokens: int, api_key: str):
     return 'error'
 
 
-def generate_prompt(instruction, input=None):
-    if input:
-        return f"""### Instruction:
-{instruction}
-
-### Input:
-{input}
-
-### Response:
-"""
-    else:
-        return f"""### Instruction:
+def generate_prompt(instruction):
+    return f"""### Instruction:
 {instruction}
 
 ### Response:
 """
 
+
+def get_json_list(file_path):
+    with open(file_path, 'r') as fcc_file:
+        json_list = json.load(fcc_file)
+    return json_list
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ChatGPT-inference.')
-    parser.add_argument('-q', '--insturction-input-file')
-    parser.add_argument('-o', '--output-inference-file')
+    parser = argparse.ArgumentParser(description='ChatGPT-based QA evaluation.')
+    parser.add_argument('-q', '--question-file-list')
+    parser.add_argument('-o', '--output-review-file')
     parser.add_argument('--api_key', type=str)
     parser.add_argument('--max-tokens', type=int, default=1024, help='maximum number of tokens produced in the output')
     parser.add_argument('--recheck', action='store_true', default=False)
     args = parser.parse_args()
 
-    question_jsons = get_json_list(args.insturction_input_file)
+    question_jsons = get_json_list(args.question_file_list)
     question_idx_list = list(range(len(question_jsons)))
     
     ques = [i['instruction'] for i in question_jsons]
-    input = [i['input'] for i in question_jsons]
     
     # if not args.recheck:
-        ### first run
+    ### first run
     ray.init()
     handles = []
     for idx in tqdm(question_idx_list):
-        user_prompt = generate_prompt(ques[idx], input[idx])
+        user_prompt = generate_prompt(ques[idx])
         
         # To avoid the rate limit set by OpenAI
         handles.append(get_eval.remote(user_prompt, args.max_tokens, args.api_key))
@@ -92,31 +84,30 @@ if __name__ == '__main__':
     
     assert len(question_jsons) == len(reviews)
     output = [{'instruction': ques[i],
-                'input': input[i],
                 'output': reviews[i]}
                 for i in question_idx_list]
     
     json_str = json.dumps(output, indent=4)
-    with open(f'{args.output_inference_file}', mode='w', encoding='utf-8') as json_file:
+    with open(f'{args.output_review_file}', mode='w', encoding='utf-8') as json_file:
         json_file.write(json_str)
     
     ray.shutdown()
             
     # else:
-        ### recheck
+    ### recheck
     old_question_idx_list = question_idx_list
     
-    reviews = get_json_list(args.output_inference_file)
+    reviews = get_json_list(args.output_review_file)
     new_question_idx_list = []
     for idx in range(len(reviews)):
-        if reviews[idx]['output']=='error':
+        if reviews[idx]['output']=='error' or reviews[idx]['output']=="":
             new_question_idx_list.append(idx)
             
     while len(new_question_idx_list) < len(old_question_idx_list):
         ray.init()
         handles = []
         for idx in tqdm(new_question_idx_list):
-            user_prompt = generate_prompt(ques[idx], input[idx])
+            user_prompt = generate_prompt(ques[idx])
             # To avoid the rate limit set by OpenAI
             handles.append(get_eval.remote(user_prompt, args.max_tokens, args.api_key))
             logger.info(f'Waiting for {REQ_TIME_GAP} seconds before sending the next request.')
@@ -128,14 +119,15 @@ if __name__ == '__main__':
             reviews[new_question_idx_list[idx]]['output'] = review
         
         json_str = json.dumps(reviews, indent=4)
-        with open(f'{args.output_inference_file}', mode='w', encoding='utf-8') as json_file:
+        with open(f'{args.output_review_file}', mode='w', encoding='utf-8') as json_file:
             json_file.write(json_str)
         
         old_question_idx_list = new_question_idx_list
         
         new_question_idx_list = []
         for idx in range(len(reviews)):
-            if reviews[idx]['output']=='error':
+            if reviews[idx]['output']=='error' or reviews[idx]['output']=="":
                 new_question_idx_list.append(idx)
 
         ray.shutdown()
+    
