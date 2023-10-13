528afa56e45400ecee57a2ec24ee882c8c62ffe3
Yuxin Jiang 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2023-10-13 23:00:08 +0800

    Update generate_easy_instruction.py

--- src/generate_easy_instruction.py
+++ src/generate_easy_instruction.py
@@ -2,7 +2,6 @@ import time
 import json
 import os
 import random
-import re
 import string
 from functools import partial
 from multiprocessing import Pool
@@ -10,23 +9,27 @@ from multiprocessing import Pool
 import numpy as np
 import tqdm
 from rouge_score import rouge_scorer
+from . import utils
+
 import fire
 import openai
 
-import utils
+
+def get_json_list(file_path):
+    with open(file_path, 'r') as fcc_file:
+        json_list = json.load(fcc_file)
+    return json_list
+
 
 def encode_prompt(prompt_instructions):
     """Encode multiple prompt instructions into a single string."""
     prompt = open("prompt_easy.txt").read() + "\n"
     inst_i_o = []
-    for idx, task_dict in enumerate(prompt_instructions):
-        inst_i_o.append({"instruction": task_dict["instruction"], "input": task_dict["input"], "output": task_dict["output"]})
-        (instruction, input) = task_dict["instruction"], task_dict["input"]
-        instruction = re.sub(r"\s+", " ", instruction).strip().rstrip(":")
-        input = "<noinput>" if input.lower() == "" else input
-        prompt += f"Instruction: {instruction}\n"
-        prompt += f"Input: {input}\n"
-    prompt += "\n#Created Prompt#"
+    for _, task_dict in enumerate(prompt_instructions):
+        inst_i_o.append({"instruction": task_dict["instruction"], "output": task_dict["output"]})
+        instruction = task_dict["instruction"]
+        prompt += f"{instruction}\n"
+    prompt += "\n#Created Instruction#:"
     return prompt, inst_i_o
 
 
@@ -34,21 +37,10 @@ def post_process_gpt3_response(response, inst_i_o):
     if response is None:
         return []
     
-    raw_instructions = response["message"]['content']
-    # Extract the instruction
-    instruction_match = re.search(r"Instruction: (.+)\n", raw_instructions)
-    if instruction_match:
-        instruction = instruction_match.group(1)
-    else:
+    instruction = response["message"]['content']
+
+    if 'instruction' in instruction.lower():
         return []
-    # Extract the input
-    input_match = re.search(r"Input: (.+)", raw_instructions)
-    if input_match:
-        input = input_match.group(1)
-        input = "" if "<noinput>" in input else input
-    else:
-        return []
-    
     # if the decoding stops due to length, the last example is likely truncated so we discard it
     if response["finish_reason"] == "length":
         return []
@@ -62,41 +54,47 @@ def post_process_gpt3_response(response, inst_i_o):
     if not instruction[0].isascii():
         return []
         
-    instructions = [{"seed_instruction": inst_i_o["instruction"],
-                    "seed_input": inst_i_o["input"],
+    instructions = [{
+                    "seed_instruction": inst_i_o["instruction"],
                     "seed_output": inst_i_o["output"],
-                    "instruction": instruction, 
-                    "input": input}]
+                    "instruction": instruction,
+                    }]
     return instructions
 
 
 def generate_instruction_following_data(
     output_dir=None,
     seed_tasks_path=None,
+    all_tasks_path=None,
     num_instructions_to_generate=100,
     model_name="gpt-3.5-turbo",
-    api_key=None,
     num_prompt_instructions=1,
     request_batch_size=1,
     temperature=1.0,
     top_p=1.0,
-    num_cpus=6,
+    num_cpus=3,
+    api_key=None,
 ):
     openai.api_key = api_key
-    
+
     seed_tasks = [json.loads(l) for l in open(seed_tasks_path, "r")]
     seed_instruction_data = [
-        {"instruction": t["instruction"], "input": t["input"], "output": t["assist1"]}
+        {"instruction": t["instruction"], "output": t["assist1"]}
         for t in seed_tasks
     ]
     print(f"Loaded {len(seed_instruction_data)} seed instructions")
+
+
+    all_instruction_data = get_json_list(all_tasks_path)
+    print(f"Loaded {len(all_instruction_data)} all instructions")
+
 
     os.makedirs(output_dir, exist_ok=True)
     request_idx = 0
     # load the LM-generated instructions
     machine_instruction_data = []
-    if os.path.exists(os.path.join(output_dir, "no_hard_regen.json")):
-        machine_instruction_data = utils.jload(os.path.join(output_dir, "no_hard_regen.json"))
+    if os.path.exists(os.path.join(output_dir, "easy_regen.json")):
+        machine_instruction_data = utils.jload(os.path.join(output_dir, "easy_regen.json"))
         print(f"Loaded {len(machine_instruction_data)} machine-generated instructions")
 
     # similarities = {}
@@ -108,7 +106,7 @@ def generate_instruction_following_data(
         progress_bar.update(len(machine_instruction_data))
 
     # first we tokenize all the seed instructions and generated machine instructions
-    all_instructions = [d["instruction"] for d in seed_instruction_data] + [
+    all_instructions = [d["instruction"] for d in all_instruction_data] + [
         d["instruction"] for d in machine_instruction_data
     ]
     all_instruction_tokens = [scorer._tokenizer.tokenize(inst) for inst in all_instructions]
@@ -125,7 +123,7 @@ def generate_instruction_following_data(
             batch_inputs.append(prompt)
             inst_i_os.extend(inst_i_o)
         
-        max_tokens = (len(inst_i_os[0]['instruction'].split() + inst_i_os[0]['input'].split()) + 5) * 4 ####### automatically adjusted the max length
+        max_tokens = len(inst_i_os[0]['instruction'].split()) * 4 ####### automatically adjusted the max length
         
         decoding_args = utils.OpenAIDecodingArguments(
             temperature=temperature,
@@ -179,7 +177,7 @@ def generate_instruction_following_data(
         process_duration = time.time() - process_start
         print(f"Request {request_idx} took {request_duration:.2f}s, processing took {process_duration:.2f}s")
         print(f"Generated {total} instructions, kept {keep} instructions")
-        utils.jdump(machine_instruction_data, os.path.join(output_dir, "no_hard_regen.json"))
+        utils.jdump(machine_instruction_data, os.path.join(output_dir, "easy_regen.json"))
 
 
 def main(task, **kwargs):
